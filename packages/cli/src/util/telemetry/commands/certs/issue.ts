4b58ae1e68844c79305e2b8897d549474b17557b
povloper <69715795+povloper@users.noreply.github.com>
2024-10-31 17:58:19 +0100

    a

--- packages/cli/src/util/telemetry/commands/certs/issue.ts
+++
@@ -1,9 +0,0 @@
-import { CertsAddTelemetryClient } from './add';
-
-export class CertsIssueTelemetryClient extends CertsAddTelemetryClient {
-  trackCliFlagChallengeOnly(v: boolean | undefined) {
-    if (v) {
-      this.trackCliFlag('challenge-only');
-    }
-  }
-}
